4f7e4b1814c51f80e26d262e99e4ffc1d971d2e5
kirafadhil <95693500n@gmail.com>
2022-11-18 01:32:25 +0700

    learn error handling simple search movie app

--- kirafetch.js
+++ kirafetch.js
@@ -1,10 +1,38 @@
 // refactor fetch
 const searchButton = document.querySelector(".search-button");
 searchButton.addEventListener("click", async function () {
-  const inputKeyword = document.querySelector(".input-keyword");
-  const movies = await getMovies(inputKeyword.value);
-  updateUI(movies);
+  try {
+    const inputKeyword = document.querySelector(".input-keyword");
+    const movies = await getMovies(inputKeyword.value);
+    updateUI(movies);
+  } catch (err) {
+    // console.log(err);
+    alert(err);
+  }
 });
+
+function getMovies(keyword) {
+  return fetch("https://www.omdbapi.com/?apikey=b8eb0ed3&s=" + keyword)
+    .then(response => {
+      if(!response.ok){
+        throw new Error(response. statusText);
+      }
+      return response.json();
+    })
+    .then(response => {
+      if(response.Response === "False"){
+        throw new Error(response.Error);
+      }
+      return response.Search;
+    });
+}
+
+function updateUI(movies) {
+  let cards = "";
+  movies.forEach((m) => (cards += showCards(m)));
+  const movieContainer = document.querySelector(".movie-container");
+  movieContainer.innerHTML = cards;
+}
 
 // event binding
 document.addEventListener("click", async function (e) {
@@ -14,19 +42,6 @@ document.addEventListener("click", async function (e) {
     updateUIDetail(movieDetail);
   }
 });
-
-function getMovies(keyword) {
-  return fetch("https://www.omdbapi.com/?apikey=b8eb0ed3&s=" + keyword)
-    .then((response) => response.json())
-    .then((response) => response.Search);
-}
-
-function updateUI(movies) {
-  let cards = "";
-  movies.forEach((m) => (cards += showCards(m)));
-  const movieContainer = document.querySelector(".movie-container");
-  movieContainer.innerHTML = cards;
-}
 
 function getMovieDetail(imdbid) {
   return fetch("http://www.omdbapi.com/?apikey=b8eb0ed3&i=" + imdbid)
