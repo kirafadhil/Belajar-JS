5b51c21291727ae0baf5bca9bcf24d878694b00e
kirafadhil <95693500n@gmail.com>
2022-11-23 02:48:39 +0700

    learn numeric enums, string enums

--- TS/src/enum.ts
+++ TS/src/enum.ts
@@ -1,12 +1,22 @@
 // enum
 
 // numeric enums
-enum Month {
-    jan,
-    feb,
-    mar,
-    apr,
-    may
-}
+// enum Month {
+//     jan = 2,
+//     feb = 100,
+//     mar,
+//     apr,
+//     may
+// }
 
+// console.log(Month.may)
+
+// string enums
+enum Month {
+    jan = 'januari',
+    feb = 'februari',
+    mar = 'maret',
+    apr = 'april',
+    may = 'mei'
+}
 console.log(Month)
