23fcb056324bc502afed76c230d3b396c6037cde
kirafadhil <95693500n@gmail.com>
2022-11-23 02:54:50 +0700

    learn function

---
+++ TS/src/function.ts
@@ -0,0 +1,16 @@
+// type data pada balikan function
+
+function getName():string {
+    return 'hello fadhil!'
+}
+console.log(getName())
+
+function getAge():number {
+    return 21
+}
+
+function printName(): void {
+    console.log('print name')
+}
+
+printName()
