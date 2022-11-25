0501f9bd528c86e02ebc5d766e92fadbf5904dd2
kirafadhil <95693500n@gmail.com>
2022-11-25 13:34:52 +0700

    learn default parameter, optional parameter

--- TS/src/function.ts
+++ TS/src/function.ts
@@ -32,4 +32,15 @@ const Add: Tambah = (val1:number, val2:number): number => {
     return val1*val2
 }
 
+// default parameter
+const fullName = (first:string, last:string = 'Naufal Fadhil'): string => {
+    return first + '' + last;
+}
+console.log(fullName('kira'))
+
+// optional parameter
+const getUmur = (val1:string, val2?:string): string => {
+    return val1+" "+val2;
+}
+console.log(getUmur('A'));
 
