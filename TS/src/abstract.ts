dbe8297cdeaced3f5354fa9e29568f577d7e6074
kirafadhil <95693500n@gmail.com>
2022-11-25 16:34:36 +0700

    learn abstract class TS

---
+++ TS/src/abstract.ts
@@ -0,0 +1,23 @@
+abstract class Vehicle {
+    abstract wheels:number;
+
+    start():void {
+        console.log('ngenggg');
+    }
+}
+
+class Car extends Vehicle {
+    wheels: number = 4;
+}
+
+class Motorcycle extends Vehicle {
+    wheels: number = 2;
+}
+
+let car = new Car();
+car.start();
+console.log(car.wheels);
+
+let motor = new Motorcycle();
+motor.start();
+console.log(motor.wheels);
