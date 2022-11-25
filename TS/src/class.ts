5b3b9f48c3aca2fb114705c700a9df83d6e5ef83
kirafadhil <95693500n@gmail.com>
2022-11-25 15:59:53 +0700

    learn inheritanceon TS

--- TS/src/class.ts
+++ TS/src/class.ts
@@ -11,8 +11,33 @@
 // atau bisa juga 
 export class User {
     constructor(public name:string, public age:number){
+        this.name = name;
+    }
+    setName(value: string):void {
+        this.name = value;
+    }
+    getName = (): string =>{
+        return this.name;
     }
 }
 
 let user = new User('fadhil', 21);
 console.log(user);
+
+// inheritance
+class Admin extends User {
+    read: boolean = true;
+    write: boolean = true;
+
+    getRole(): {read:boolean, write:boolean}{
+        return {
+            read: this.read,
+            write: this.write
+        };
+    }
+}
+
+let admin = new Admin('baby',20);
+admin.getRole();
+admin.getName();
+admin.setName('karin')
