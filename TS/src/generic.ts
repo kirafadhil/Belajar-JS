c5440c4f299d297634e7d9713be833d821733b3e
kirafadhil <95693500n@gmail.com>
2022-11-26 07:09:55 +0700

    learn generic type data TS

---
+++ TS/src/generic.ts
@@ -0,0 +1,20 @@
+function getData(value:any){
+    return value;
+}
+
+console.log(getData('fadhil').length);
+console.log(getData(123).length);
+
+// generic type data
+function getInfo<T>(value: T){
+    return value;
+}
+console.log(getInfo('baby violeta'));
+console.log(getInfo(123).length);
+
+// generic on arrow function
+const arrowFunc = <T>(value:T) => {
+
+}
+ 
+
